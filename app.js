7e522880ef57f990deade136f1c90168fa678f64
Aaron doran <aarondoran@outlook.ie>
2023-12-10 14:24:24 +0000

    Update app.js

--- app.js
+++ app.js
@@ -39,3 +39,11 @@ function login() {
       errorMessageContainer.innerHTML = errorMessage;
     });
 }
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("User is signed in:", user);
+  } else {
+    console.log("User is signed out.");
+  }
+});
